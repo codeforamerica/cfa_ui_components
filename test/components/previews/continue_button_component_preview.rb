cf4de0d9b39f2dfd6e3fa0bd5cbbc41c7e9ed015
Mike Rotondo <mrotondo@codeforamerica.org>
2025-10-16 18:07:35 -0700

    add preview scenario for using continue button with custom label

--- test/components/previews/continue_button_component_preview.rb
+++ test/components/previews/continue_button_component_preview.rb
@@ -2,4 +2,8 @@ class ContinueButtonComponentPreview < FormComponentPreview
   def default
     render(ContinueButtonComponent.new(form: form))
   end
+
+  def custom_label
+    render(ContinueButtonComponent.new(form: form, label: I18n.t("submit")))
+  end
 end
