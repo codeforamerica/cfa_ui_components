9222d932210db57ecf4e21d783437cdbac44fe9c
Mike Rotondo <mrotondo@codeforamerica.org>
2025-10-17 21:10:26 -0700

    automatically symlink our stylesheet into the host app's convention stylesheets dir

--- lib/cfa_ui_components/engine.rb
+++ lib/cfa_ui_components/engine.rb
@@ -7,12 +7,9 @@ module CfaUiComponents
 
     initializer "cfa_ui_components.assets" do |app|
       if app.config.respond_to?(:assets)
-        # I believe these are unnecessary, since Tailwind's @import simply looks for relative paths
-        # But I think we may still want to use the engine to do something tricky like symlink the stylesheet into the
-        # app's stylesheets directory, so I'm keeping it for now.
-
-        # app.config.assets.paths += [ "#{root}/app/stylesheets" ]
-        # app.config.assets.precompile += ["cfa_ui_components.tailwind.css"]
+        source_path = "#{root}/app/assets/stylesheets/cfa_ui_components.tailwind.css"
+        dest_path = "#{Rails.root}/app/assets/stylesheets/cfa_ui_components.tailwind.css"
+        File.symlink(source_path, dest_path) unless File.exist? dest_path
       end
     end
   end
