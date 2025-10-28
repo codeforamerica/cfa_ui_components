06dd750f031a8bd205c209685bde321d691a3326
Mike Rotondo <mrotondo@codeforamerica.org>
2025-10-27 18:51:16 -0700

    space out card contents

--- test/components/previews/card_component_preview.rb
+++ test/components/previews/card_component_preview.rb
@@ -1,7 +1,8 @@
 class CardComponentPreview < ViewComponent::Preview
   def default
     render(CardComponent.new) do
-      content_tag :p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper at diam at lobortis. Quisque quis molestie sem. Maecenas pellentesque."
+      content_tag(:div, content_tag(:strong, "Sample form card")) +
+        content_tag(:div, content_tag(:p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat ornare elit a vestibulum. Etiam eget sodales ipsum. Etiam accumsan neque erat."))
     end
   end
 end
