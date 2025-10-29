2b918aae9faaa1ca01d86b02a2d8bcb916a0556b
Mike Rotondo <mrotondo@codeforamerica.org>
2025-10-28 17:34:27 -0700

    simplify card component preview html

--- test/components/previews/card_component_preview.rb
+++ test/components/previews/card_component_preview.rb
@@ -1,8 +1,8 @@
 class CardComponentPreview < ViewComponent::Preview
   def default
     render(CardComponent.new) do
-      content_tag(:div, content_tag(:strong, "Sample form card")) +
-        content_tag(:div, content_tag(:p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat ornare elit a vestibulum. Etiam eget sodales ipsum. Etiam accumsan neque erat."))
+      content_tag(:p, content_tag(:strong, "Sample form card")) +
+        content_tag(:p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat ornare elit a vestibulum. Etiam eget sodales ipsum. Etiam accumsan neque erat.", class: "m-0")
     end
   end
 end
